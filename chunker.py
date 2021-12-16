078d7a3f5d906d3a5f432e01a79ff5b8bfea2068
sixo <michael.bowen@full360.com>
2021-12-16 12:49:51 -0800

    chunker complete

--- chunker.py
+++ chunker.py
@@ -9,34 +9,33 @@ import regex as re
 # Abstract
 # This will chunk sql statements to be run but taking an input list and parsing for statement delimiters
 
-def chunker(stream):
-    l = len(stream)
-
-
 def get_chunks():
     i = 0
     c = 0
     sql = {}
-    chunk = {}
-    lines = []
+    h = {}
+    cmds = []
     with open('scripts/blah.sql', 'r') as file:
         for line in file:
 
-            logging.info("line: {} {}".format(i,line.strip()))
-            sql[i] = line.strip()
-            lines.append(sql)
+            logging.info("chunk: {} line: {} {}".format(c,i,line.strip()))
+            h = {'chunk':c, 'line': i, 'sql': line.strip()}
 
             m0 = re.search(r";$",line)
             if m0 != None:         
-                chunk[c] = lines
-                c+=1
-                lines.clear()
-            i+=1
+                c+=1  #new chunk
+                i = 0 #reset line
+            else:
+                i+=1 #another line in the same chunk
         
+            cmds.append(h)
         print("number of chunks: ", c)
         file.close()
-    
-    return chunk
+
+    # for cmd in cmds:
+    #     print(cmd)
+
+    return cmds
 
 
 def query_chunks(qset):
@@ -49,9 +48,29 @@ def query_chunks(qset):
 lname = 'log/chunker.log'
 logging.basicConfig(filename=lname, level=logging.INFO, format='%(asctime)s %(message)s')
 h = get_chunks()
+# print(h)
 
-for qset in h:
-    print(qset)
-    for v in qset.values():
-        print(v)
+
+sql =''
+for cmd in h:
+    l = cmd['line']
+    s = cmd['sql']
+    c = cmd['chunk']
+    if l == 0:       
+        print('----chunk: ',c)
+        sql+= s
+        print(s)
+        process_sql(sql)
+        sql =''
+    else:
+        sql+= s
+        print(s)
+
+    
+
+
+# for qset in h:
+#     print(qset)
+#     for v in qset.values():
+#         print(v)
 
