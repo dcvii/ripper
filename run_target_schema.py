4ba9a63f76ba47e5b4dde588a98db4dbd986f686
sixo <michael.bowen@full360.com>
2022-03-04 15:54:07 -0800

    added table_only

---
+++ run_target_schema.py
@@ -0,0 +1,21 @@
+import vertica_python
+import os
+import sys
+import logging
+import pandas as pd
+
+from ripper.sql_runner import chunkify, run_multi_sql, run_single_file_sql, run_migration_table
+
+
+lname = 'log/migrate_tevaQA_table_only.log'
+bucket_key = os.getenv('TARGET_BUCKET_KEY')
+logging.basicConfig(filename=lname, level=logging.INFO, format='%(asctime)s %(message)s')
+
+
+cmd_set = chunkify('scripts/tevaQA_table_only_catalog.sql')
+config = {'in_fspec': 'sql/get_all_csv.sql', 'log': lname, 'export_type': 'csv', 'conn_type': 'tgt', 'bucket_key': bucket_key}
+result_set = run_multi_sql(cmd_set,config)
+
+
+
+
