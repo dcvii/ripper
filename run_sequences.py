74a5cf00a6baf43b0d959c77699091bf862baee5
sixo <michael.bowen@full360.com>
2022-04-12 14:36:02 -0700

    rid hardcoded bucket keys

--- run_sequences.py
+++ run_sequences.py
@@ -6,7 +6,7 @@ import logging
 from ripper.sql_runner import chunkify, run_multi_sql, run_single_file_sql,run_single_file_commit_sql, run_migration_table
 
 bucket_key = os.getenv('TARGET_BUCKET_KEY')
-lname = 'log/migrate_'+bucket_key+'_tevaQA_run_sequences.log'
+lname = 'log/migrate_'+bucket_key+'_run_sequences.log'
 logging.basicConfig(filename=lname, level=logging.INFO, format='%(asctime)s %(message)s')
 
 cset = chunkify('scripts/'+bucket_key+'_out_sequences.sql')
