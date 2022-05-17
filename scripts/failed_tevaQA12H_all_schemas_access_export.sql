89f65391be0cd30928d574e5068ee7aa18954fe0
mike bowen <michael.bowen@full360.com>
2022-05-16 18:10:57 -0700

    ran mdm - errors not mailed to me

--- scripts/failed_tevaQA12H_all_schemas_access_export.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE metadata.tmp_DSC_debug OWNER TO Databurst_Updt;
-GRANT TRUNCATE ON metadata.tmp_DSC_debug TO Databurst_Updt WITH GRANT OPTION;
-GRANT REFERENCES ON metadata.tmp_DSC_debug TO Databurst_Updt WITH GRANT OPTION;
-GRANT DELETE ON metadata.tmp_DSC_debug TO Databurst_Updt WITH GRANT OPTION;
-GRANT UPDATE ON metadata.tmp_DSC_debug TO Databurst_Updt WITH GRANT OPTION;
-GRANT SELECT ON metadata.tmp_DSC_debug TO Databurst_Updt WITH GRANT OPTION;
-GRANT INSERT ON metadata.tmp_DSC_debug TO Databurst_Updt WITH GRANT OPTION;
